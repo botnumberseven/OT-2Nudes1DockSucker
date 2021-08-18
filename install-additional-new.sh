9f758dcc0a63b74c09a34f319df012e987617528
calr0x <calr0x@gmail.com>
2021-08-17 19:09:47 -0500

    .

--- install-additional-new.sh
+++ install-additional-new.sh
@@ -13,12 +13,17 @@ fi
 
 mkdir -p $DEST_DIR
 
+cp /root/.origintrail_noderc $DEST_DIR
 cp /root/OT-2Nudes1DockSucker/otnode.service /lib/systemd/system/$NODE_ID.service
 
 sed -i "s|otnodeX|$NODE_ID|g" /lib/systemd/system/$NODE_ID.service
 
 cp /root/OT-2Nudes1DockSucker/start-script $DEST_DIR/$NODE_ID
 sed -i "s|otnodeX|$NODE_ID|g" $DEST_DIR/$NODE_ID
+
+cd /ot-node/current/
+npm run setup
+mv /root/.origintrail_noderc/mainnet/system.db $DEST_DIR
 
 systemctl enable $NODE_ID
 #systemctl start $NODE_ID
